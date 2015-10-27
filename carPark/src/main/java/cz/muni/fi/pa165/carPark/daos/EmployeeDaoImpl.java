1f54d11a2693c400da67fdcb2c24ef90c2b07dc4
Erik Hasprunar <erik.hasprunar@gmail.com>
2015-10-27 20:16:14 +0100

    -"persistance.xml" -> "persistence.xml" -update method removed from EmployeeDao -Employee entity finished. (Ready for discussion) -EmployeeDaoImpl almost finished (dependency/interface problem)
    +chcem pouzit v dao ten kontajnerovy EntityManager, potrebujem si to este prestudovat ci/co tam treba nastavit v kontexte (mozno to ani pre tento milestone netreba ak staci Build aplikacie)

--- carPark/src/main/java/cz/muni/fi/pa165/carPark/daos/EmployeeDaoImpl.java
+++ carPark/src/main/java/cz/muni/fi/pa165/carPark/daos/EmployeeDaoImpl.java
@@ -1,45 +1,39 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pa165.carPark.daos;
 
 import cz.muni.fi.pa165.carPark.entities.Employee;
 import java.util.List;
-import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 /**
  *
- * @author xcmarko
+ * @author Erik Hasprunar
  */
+@Transactional
 public class EmployeeDaoImpl implements EmployeeDao{
+    
+    @PersistenceContext
+    private EntityManager em;
 
-    public EmployeeDaoImpl(EntityManagerFactory emf) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    @Override
+    public void createEmployee(Employee employee) {
+        em.persist(employee);
     }
 
     @Override
-    public void createCar(Employee employee) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void deleteEmployee(Employee employee) {
+        em.remove(employee);
     }
 
     @Override
-    public void deleteCar(Employee employee) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Employee findEmployeeById(Long id) {
+        return em.find(Employee.class, id);
     }
 
     @Override
-    public void updateCar(Employee employee) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Employee findCarById(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public List<Employee> findAllCars() {
+    public List<Employee> findAllEmployees() {
+        //TODO !!!interface problem!!!
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
