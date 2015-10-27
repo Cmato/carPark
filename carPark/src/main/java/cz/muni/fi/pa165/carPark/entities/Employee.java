f330d485ce72d3d6042bc8b1e138975b1fb2985d
Erik Hasprunar <erik.hasprunar@gmail.com>
2015-10-27 19:14:03 +0100

    1 - Employee in Progress

--- carPark/src/main/java/cz/muni/fi/pa165/carPark/entities/Employee.java
+++ carPark/src/main/java/cz/muni/fi/pa165/carPark/entities/Employee.java
@@ -4,10 +4,26 @@
  */
 package cz.muni.fi.pa165.carPark.entities;
 
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
 /**
  *
- * @author xcmarko
+ * @author Erik Hasprunar
  */
+@Entity
 public class Employee {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
     
+    private String name;
+    
+    private Date birth;
+    
+    private String idCardNumber;
 }
