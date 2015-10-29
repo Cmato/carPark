20fe7ee4ec3b8412e675f0ebf28d97376d4e2004
_CJ_ <Inaros2010@gmail.com>
2015-10-29 13:12:15 +0100

    Add implementation of ReservationDao

---
+++ carPark/src/main/java/cz/muni/fi/pa165/carPark/daos/ReservationDaoImpl.java
@@ -0,0 +1,56 @@
+package cz.muni.fi.pa165.carPark.daos;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import cz.muni.fi.pa165.carPark.entities.Employee;
+import cz.muni.fi.pa165.carPark.entities.Reservation;
+
+/**
+*
+* @author xruzic16
+*/
+@Repository
+@Transactional
+public class ReservationDaoImpl implements ReservationDao {
+
+	@PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void create(Reservation reservation) {
+        em.persist(reservation);
+    }
+
+    @Override
+    public List<Reservation> findAll() {     
+        return em.createQuery("SELECT r FROM Reservation r", Reservation.class).getResultList();
+    }
+
+    @Override
+    public List<Reservation> findByEmployee(Employee employee) {
+        return em.createQuery("SELECT r FROM Reservation r WHERE r.employee.id = :id", Reservation.class)
+                .setParameter("id", employee.getId()).getResultList();
+    }
+
+    @Override
+    public Reservation findById(Long id) {
+        return em.find(Reservation.class, id);
+    }
+
+    @Override
+    public Reservation update(Reservation reservation) {
+        return em.merge(reservation);
+    }
+
+    @Override
+    public void remove(Reservation reservation) {
+        em.remove(reservation);
+    }
+
+}
