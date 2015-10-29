b14a537cd2739985a59cb7119463fd0b0da803b1
_CJ_ <Inaros2010@gmail.com>
2015-10-29 12:49:44 +0100

    Adding ReservationDao interface

---
+++ carPark/src/main/java/cz/muni/fi/pa165/carPark/daos/ReservationDao.java
@@ -0,0 +1,59 @@
+package cz.muni.fi.pa165.carPark.daos;
+
+import java.util.List;
+
+import cz.muni.fi.pa165.carPark.entities.Employee;
+import cz.muni.fi.pa165.carPark.entities.Reservation;
+
+/**
+ * Class represents basic CRUD operations with the entity reservation.
+ * 
+ * @author xruzic16
+ */
+
+public interface ReservationDao {
+	/**
+	 * Inserts reservation into the database
+	 * 
+	 * @param reservation to be inserted
+	 */
+	public void create(Reservation reservation);
+
+	/**
+	 * Retrieves all reservation from the database
+	 * 
+	 * @return List of reservations
+	 */
+	public List<Reservation> findAll();
+
+	/**
+	 * Retrieves reservations for specific employee
+	 * 
+	 * @param employee specific for the reservation
+	 * @return List of reservations
+	 */
+	public List<Reservation> findByEmployee(Employee employee);
+
+	/**
+	 * Retrieves a reservation with specific id from the database
+	 * 
+	 * @param id of the reservation
+	 * @return reservation with the specific id
+	 */
+	public Reservation findById(Long id);
+
+	/**
+	 * Update specific reservation in the database
+	 * 
+	 * @param reservation to be updated
+	 * @return updated reservation
+	 */
+	public Reservation update(Reservation reservation);
+
+	/**
+	 * Delete specific reservation from the database
+	 * 
+	 * @param reservation to be deleted
+	 */
+	public void remove(Reservation reservation);
+}
