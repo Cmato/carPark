58244dc8b42f61dc55e0cdefc4221ee5655d4cfe
_CJ_ <Inaros2010@gmail.com>
2015-11-27 00:14:53 +0100

    reservation entity fix, adding state functionality

--- carPark-persistence/src/main/java/cz/muni/fi/pa165/entities/Reservation.java
+++ carPark-persistence/src/main/java/cz/muni/fi/pa165/entities/Reservation.java
@@ -109,6 +109,8 @@ public class Reservation {
 		result = prime * result + ((startingDate == null) ? 0 : startingDate.hashCode());
 		result = prime * result + ((endingDate == null) ? 0 : endingDate.hashCode());
 		result = prime * result + ((employee == null) ? 0 : employee.hashCode());
+		result = prime * result + ((car == null) ? 0 : car.hashCode());
+		result = prime * result + ((reservationState == null) ? 0 : reservationState.hashCode());
 		return result;
 	}
 
@@ -118,29 +120,41 @@ public class Reservation {
 			return true;
 		if (obj == null)
 			return false;
-		if (!(obj instanceof Rental))
+		if (!(obj instanceof Reservation))
 			return false;
-		Rental other = (Rental) obj;
+		Reservation other = (Reservation) obj;
 		if (startingDate == null) {
 			if (other.getStartingDate() != null)
 				return false;
 		} else if (!startingDate.equals(other.getStartingDate()))
 			return false;
 		if (endingDate == null) {
-			if (other.getEstimatedReturnDate() != null)
+			if (other.getEndingDate() != null)
 				return false;
-		} else if (!endingDate.equals(other.getEstimatedReturnDate()))
+		} else if (!endingDate.equals(other.getEndingDate()))
 			return false;
 		if (employee == null) {
 			if (other.getEmployee() != null)
 				return false;
 		} else if (!this.getEmployee().equals(other.getEmployee()))
 			return false;
+		if (car == null) {
+			if (other.getCar() != null)
+				return false;
+		} else if (!this.getCar().equals(other.getCar()))
+			return false;
+		if (reservationState == null) {
+			if (other.getReservationState() != null)
+				return false;
+		} else if (!this.getReservationState().equals(other.getReservationState()))
+			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Rental " + id + " -- Employee: " + employee.getName() + " From: " + DateFormater.format(startingDate) + " To: " + DateFormater.format(endingDate);
+		return "Reservation " + id + " -- Employee: " + employee.getName() + " Car: " + car.getName() +
+				" From: " + DateFormater.format(startingDate) + " To: " + DateFormater.format(endingDate)+
+				" State: " + reservationState.toString();
 	}
 }
